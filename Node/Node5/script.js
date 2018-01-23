009d95fa17426476cc3d845872d7505b8a62a949
Jabier Espinal <jabierespinal@gmail.com>
2018-01-23 17:44:38 +0000

    added node 5

---
+++ Node/Node5/script.js
@@ -0,0 +1,49 @@
+//app.js 
+var express = require('express');
+var app = express();
+//added below code
+app.get('/tweets', function(req, res) {
+  res.sendFile(__dirname + "/tweets.html");
+});
+
+app.listen(8080);
+
+//var url = require('url');
+
+var options = {
+  protocol: "http:",
+  host: "search.twitter.com",
+  pathname: '/search.json',
+  query: { q: "codeschool"}
+};
+
+var searchURL = url.format(options);
+//added code below
+var request = require('request');
+request(searchURL, function(err, response, body) {
+  console.log(body);
+});
+
+
+//------------------------------------
+var url = require('url');
+var request = require('request');
+
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+  request(searchURL).pipe(res);
+});
+var options = {
+  protocol: "http:",
+  host: "search.twitter.com",
+  pathname: '/search.json',
+  query: {
+    q: "codeschool"
+  }
+};
+
+var searchURL = url.format(options);
+
+var app; // Create server here
+app.listen(8080);
